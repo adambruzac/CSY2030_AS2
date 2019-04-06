972ca2ea854498b4fa2c8f93d4c87ae4fba59e6a
adambruzac <adam.bruzac91@gmail.com>
2019-04-06 02:45:40 +0100

    Add files via upload

--- src/application/department.java
+++ src/application/department.java
@@ -1,15 +1,25 @@
 package application;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class  department implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private String departmentID;
 	private String departmentName;
 	
+
 
 	public void setDepartmentID(String departmentID) {
 		this.departmentID = departmentID;
@@ -29,12 +39,15 @@ public class  department implements Serializable{
 				+ "]";
 	}
 	
-	
+
+
+	/*
 	private void writeObject(ObjectOutputStream os) throws IOException, ClassNotFoundException{
 		try {
+			
 			os.defaultWriteObject();
-			os.writeObject(getDepartmentID());
-			os.writeObject(getDepartmentName());
+			os.writeObject(departmentID);
+			os.writeObject(departmentName);
 		} catch(Exception e) {
 			e.printStackTrace();;
 		}
@@ -49,9 +62,7 @@ public class  department implements Serializable{
 	 
 	  } catch (Exception e) { 
 		  e.printStackTrace(); 
-		  }
-	 }
-	
-	
-	
+	    
+	  }
+	 }*/
 }
